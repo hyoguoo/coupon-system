92363bd1289a9ce0fa9715e355b4b6242f6f8297
hyoguoo <hyoguoo@gmail.com>
2024-06-18 20:27:18 +0900

    feat: 사용자당 하나의 쿠폰만 발행하는 메서드 추가

--- api/src/main/java/study/api/service/ApplyService.java
+++ api/src/main/java/study/api/service/ApplyService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import study.api.domain.Coupon;
 import study.api.producer.CouponCreateProducer;
+import study.api.repository.AppliedUserRepository;
 import study.api.repository.CouponRedisRepository;
 import study.api.repository.CouponRepository;
 
@@ -16,6 +17,7 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponRedisRepository couponRedisRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
     @Transactional
     public void apply(Long userId) {
@@ -41,6 +43,23 @@ public class ApplyService {
 
     @Transactional
     public void applyWithKafkaProducer(Long userId) {
+        long count = couponRedisRepository.increment();
+
+        if (count > MAX_COUPON_LIMIT) {
+            return;
+        }
+
+        couponCreateProducer.create(userId);
+    }
+
+    @Transactional
+    public void applyKafkaProducerOnlyOneCoupon(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         long count = couponRedisRepository.increment();
 
         if (count > MAX_COUPON_LIMIT) {
