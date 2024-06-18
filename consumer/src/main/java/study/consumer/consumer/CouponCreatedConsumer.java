c14eff0a09ec8492ef66f6312eaeb62cb34ec690
hyoguoo <hyoguoo@gmail.com>
2024-06-18 20:36:41 +0900

    feat: 쿠폰 저장 실패 시 로깅 및 실패 이벤트 저장

--- consumer/src/main/java/study/consumer/consumer/CouponCreatedConsumer.java
+++ consumer/src/main/java/study/consumer/consumer/CouponCreatedConsumer.java
@@ -1,19 +1,29 @@
 package study.consumer.consumer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import study.consumer.domain.Coupon;
+import study.consumer.domain.FailedEvent;
 import study.consumer.repository.CouponRepository;
+import study.consumer.repository.FailedEventRepository;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
+    private final FailedEventRepository failedEventRepository;
 
     @KafkaListener(topics = "${kafka.topic.coupon-create}", groupId = "${kafka.consumer.group-id}")
     public void listener(Long couponId) {
-        couponRepository.save(new Coupon(couponId));
+        try {
+            couponRepository.save(new Coupon(couponId));
+        } catch (Exception e) {
+            log.error("failed to create coupon:: couponId={}", couponId, e);
+            failedEventRepository.save(new FailedEvent(couponId));
+        }
     }
 }
