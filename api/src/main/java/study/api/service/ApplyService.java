8363e904fd28f705306554b0d7a345cb90a75064
hyoguoo <hyoguoo@gmail.com>
2024-06-18 16:31:24 +0900

    feat: RedisRepository를 통한 재고 체크 로직이 포함된 메서드 추가

--- api/src/main/java/study/api/service/ApplyService.java
+++ api/src/main/java/study/api/service/ApplyService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import study.api.domain.Coupon;
+import study.api.repository.CouponRedisRepository;
 import study.api.repository.CouponRepository;
 
 @Service
@@ -12,12 +13,24 @@ public class ApplyService {
 
     public static final int MAX_COUPON_LIMIT = 100;
     private final CouponRepository couponRepository;
+    private final CouponRedisRepository couponRedisRepository;
 
     @Transactional
     public void apply(Long userId) {
         long count = couponRepository.count();
 
-        if (count >= MAX_COUPON_LIMIT) {
+        if (count > MAX_COUPON_LIMIT) {
+            return;
+        }
+
+        couponRepository.save(new Coupon(userId));
+    }
+
+    @Transactional
+    public void applyWithRedisRepository(Long userId) {
+        Long count = couponRedisRepository.increment();
+
+        if (count > MAX_COUPON_LIMIT) {
             return;
         }
 
