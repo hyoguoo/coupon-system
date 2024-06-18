483ea1d82a8612c7fbc0cba48a8f176e47f5397b
hyoguoo <hyoguoo@gmail.com>
2024-06-18 19:27:29 +0900

    feat: Kafka Producer를 이용하는 서비스 메서드 추가

--- api/src/main/java/study/api/service/ApplyService.java
+++ api/src/main/java/study/api/service/ApplyService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import study.api.domain.Coupon;
+import study.api.producer.CouponCreateProducer;
 import study.api.repository.CouponRedisRepository;
 import study.api.repository.CouponRepository;
 
@@ -14,6 +15,7 @@ public class ApplyService {
     public static final int MAX_COUPON_LIMIT = 100;
     private final CouponRepository couponRepository;
     private final CouponRedisRepository couponRedisRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
     @Transactional
     public void apply(Long userId) {
@@ -35,5 +37,16 @@ public class ApplyService {
         }
 
         couponRepository.save(new Coupon(userId));
+    }
+
+    @Transactional
+    public void applyWithKafkaProducer(Long userId) {
+        long count = couponRedisRepository.increment();
+
+        if (count > MAX_COUPON_LIMIT) {
+            return;
+        }
+
+        couponCreateProducer.create(userId);
     }
 }
