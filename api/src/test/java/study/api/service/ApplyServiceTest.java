f55fb16b4789aaaa5af1d7924089cc6c32ba5059
hyoguoo <hyoguoo@gmail.com>
2024-06-18 16:31:59 +0900

    test: Redis를 사용하는 여러 사용자에 대한 테스트 추가

--- api/src/test/java/study/api/service/ApplyServiceTest.java
+++ api/src/test/java/study/api/service/ApplyServiceTest.java
@@ -74,6 +74,37 @@ class ApplyServiceTest {
         countDownLatch.await();
 
         // then
+        assertThat(couponRepository.count()).isNotEqualTo(MAX_COUPON_LIMIT);
+    }
+
+    @Test
+    @DisplayName("여러 명이 쿠폰을 발급한다. (Redis)")
+    void apply_with_multiple_users_with_redis() throws InterruptedException {
+        // given
+        final int totalUsers = 1000;
+        final int threadCount = 32;
+
+        // when
+        CountDownLatch countDownLatch;
+
+        try (ExecutorService executorService = Executors.newFixedThreadPool(threadCount)) {
+            countDownLatch = new CountDownLatch(totalUsers);
+
+            for (int i = 0; i < totalUsers; i++) {
+                long userId = i;
+                executorService.submit(() -> {
+                    try {
+                        applyService.applyWithRedisRepository(userId);
+                    } finally {
+                        countDownLatch.countDown();
+                    }
+                });
+            }
+        }
+
+        countDownLatch.await();
+
+        // then
         assertThat(couponRepository.count()).isEqualTo(MAX_COUPON_LIMIT);
     }
 }
