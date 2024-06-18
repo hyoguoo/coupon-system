d04809e2c8d6d9343940835afc7927da906019df
hyoguoo <hyoguoo@gmail.com>
2024-06-18 19:59:14 +0900

    feat: coupon-create listen 시 저장하는 로직 추가

--- consumer/src/main/java/study/consumer/consumer/CouponCreatedConsumer.java
+++ consumer/src/main/java/study/consumer/consumer/CouponCreatedConsumer.java
@@ -1,13 +1,19 @@
 package study.consumer.consumer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import study.consumer.domain.Coupon;
+import study.consumer.repository.CouponRepository;
 
 @Component
+@RequiredArgsConstructor
 public class CouponCreatedConsumer {
+
+    private final CouponRepository couponRepository;
 
     @KafkaListener(topics = "${kafka.topic.coupon-create}", groupId = "${kafka.consumer.group-id}")
     public void listener(Long couponId) {
-        System.out.println("Coupon created: " + couponId);
+        couponRepository.save(new Coupon(couponId));
     }
 }
